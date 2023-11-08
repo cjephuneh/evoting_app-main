4ec5f5abedd33fb87f447649f871a386fcba7ec7
cjephuneh <calebjephuneh@gmail.com>
2023-11-08 11:47:50 +0300

    changes

--- myvoting/settings.py
+++ myvoting/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g=jzhv5zv8nxar2z0jpv#jv-$o*p(+e597*wgkau$gc&7v*m4e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "my-voting-app-q6ef.onrender.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "evoting-i0hu.onrender.com"]
 
 
 # Application definition
